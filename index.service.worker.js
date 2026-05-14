b31e3cfbadcc6495e40d864017dac6840cdfefb7
WhiteKnight <114160109+Blacki005@users.noreply.github.com>
2026-05-14 18:05:42 +0200

    bugfix, added sound and effects for levels 3,4

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1778752988|10548258382';
+const CACHE_VERSION = '1778774710|11594042726';
 /** @type {string} */
 const CACHE_PREFIX = 'stc_naborova_hra-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
