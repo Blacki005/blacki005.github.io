1124e1ba4e9d954d695402a77e1592db1bc025a0
WhiteKnight <114160109+Blacki005@users.noreply.github.com>
2026-05-14 12:03:31 +0200

    meridias beacon added; removed herobrine

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1778749142|6701561654';
+const CACHE_VERSION = '1778752988|10548258382';
 /** @type {string} */
 const CACHE_PREFIX = 'stc_naborova_hra-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
