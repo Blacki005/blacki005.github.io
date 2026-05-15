2c2f41a19aaaa47aee66ba8125a263d0c4987219
WhiteKnight <114160109+Blacki005@users.noreply.github.com>
2026-05-15 09:43:23 +0200

    added volume slider in pause screen

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1778829087|16583166857';
+const CACHE_VERSION = '1778830979|18475286016';
 /** @type {string} */
 const CACHE_PREFIX = 'stc_naborova_hra-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
