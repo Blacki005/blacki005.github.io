b53fa22fa6bcec15bce669694b8e6293d4c069c7
WhiteKnight <114160109+Blacki005@users.noreply.github.com>
2026-05-15 09:11:58 +0200

    added dubbing

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1778774710|11594042726';
+const CACHE_VERSION = '1778829087|16583166857';
 /** @type {string} */
 const CACHE_PREFIX = 'stc_naborova_hra-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
