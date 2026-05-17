78dbe1dd9d41a0172927eaa3a6cebde55261804d
WhiteKnight <114160109+Blacki005@users.noreply.github.com>
2026-05-17 12:10:40 +0200

    bugfix, final polishing

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1778929337|12836598886';
+const CACHE_VERSION = '1779012614|11130864307';
 /** @type {string} */
 const CACHE_PREFIX = 'stc_naborova_hra-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
