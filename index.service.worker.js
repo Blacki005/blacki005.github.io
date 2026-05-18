2132778ef128439d67293497b5151dadf479f41e
WhiteKnight <114160109+Blacki005@users.noreply.github.com>
2026-05-18 20:55:39 +0200

    explosion effects for some weapons, new projectile for document enemies

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1779125375|19199103976';
+const CACHE_VERSION = '1779130514|24338961995';
 /** @type {string} */
 const CACHE_PREFIX = 'stc_naborova_hra-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
