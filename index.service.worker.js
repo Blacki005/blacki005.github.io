4a2d9d1d1995ae670863f927a2f79a325405bee0
WhiteKnight <114160109+Blacki005@users.noreply.github.com>
2026-05-18 21:51:28 +0200

    fixed bug with shop

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1779130514|24338961995';
+const CACHE_VERSION = '1779133536|27360643340';
 /** @type {string} */
 const CACHE_PREFIX = 'stc_naborova_hra-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
