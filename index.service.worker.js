b0dd2e1836dafec57b2772f95e482de1cde0cf5b
WhiteKnight <114160109+Blacki005@users.noreply.github.com>
2026-05-19 15:13:50 +0200

    added boss chrudimak

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1779133536|27360643340';
+const CACHE_VERSION = '1779195697|1054889883';
 /** @type {string} */
 const CACHE_PREFIX = 'stc_naborova_hra-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
